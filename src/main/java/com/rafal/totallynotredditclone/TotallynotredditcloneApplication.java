55a2343f74cf17f36a72badbc8033ee739edcaed
mafiozorek <orchowski.raf@gmail.com>
2020-06-26 15:12:05 +0200

    Initial commit

---
+++ src/main/java/com/rafal/totallynotredditclone/TotallynotredditcloneApplication.java
@@ -0,0 +1,13 @@
+package com.rafal.totallynotredditclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotallynotredditcloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TotallynotredditcloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rafal/totallynotredditclone/TotallynotredditcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafal.totallynotredditclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotallynotredditcloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
